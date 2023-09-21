106d43940274619fe209cbd4aa8b2408e8886457
Minjoo Cho <minjoolisa.cho@gmail.com>
2023-09-21 11:58:26 +0100

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,57 @@ import Link from "next/link";
 const Page = async () => {
   return (
     <section className="relative w-full h-full bg-cyan-600">
-      <div className="container flex flex-col max-w-3xl mx-auto py-20 px-4 py-4 sm:px-6">
+      <div className="container flex flex-col h-full justify-around max-w-3xl mx-auto px-4 py-4 sm:px-6">
         <div className="text-center pb-12 md:pb-16">
           <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4">
             D3JS Experiments
           </h1>
-          <p className="text-lg  max-w-md mx-auto">
+          <p className="text-lg max-w-md mx-auto">
             This page explores the different ways to visualise data despending
             on the number of datapoints
           </p>
+          <p className="inline-block max-w-none mt-5 underline font-bold text-slate-300">
+            <a
+              className="text-lg max-w-md mx-auto"
+              href="https://github.com/mj-life-is-once/scatter-plot"
+            >
+              Source Code
+            </a>
+          </p>
         </div>
-        <div className="flex max-lg:flex-row max-md:flex-col jusitfy-center flex-wrap gap-4 [&>h1]: lg:text-3xl md:text-3xl sm:text-3xl font-bold leading-tighter tracking-tighter mb-4">
-          {/* <Card className="bg-slate-900">
-            <h1>SVG Chart</h1>
-          </Card> */}
+        <div className="relative flex max-lg:flex-row max-md:flex-col jusitfy-center flex-wrap gap-4 [&>h1]: lg:text-3xl md:text-3xl sm:text-3xl font-bold leading-tighter tracking-tighter mb-4">
           <Card className="bg-slate-900">
             <h1 className="pb-5">Canvas Chart with SVG domains</h1>
-            <Link href="/canvas">Explore</Link>
+            <p className="font-bold pb-10 text-lg">~10k datapoints</p>
+            <Link
+              className="absolute right-10 bottom-5 inline-block"
+              href="/canvas"
+            >
+              Explore
+            </Link>
+            <p></p>
           </Card>
           <Card className="bg-slate-900">
             <h1 className="pb-5">WebGL Chart with SVG domains</h1>
-            <Link href="/webgl">Explore</Link>
+            <p className="pb-10 text-lg">~1M datapoints</p>
+            <Link
+              className="absolute right-10 bottom-5 inline-block"
+              href="/webgl"
+            >
+              Explore
+            </Link>
           </Card>
+        </div>
+        <div className="text-center">
+          <ul className="text-lg">
+            Resource
+            <li className="text-sm pt-2 underline">
+              <a href="https://blog.scottlogic.com/2020/05/01/rendering-one-million-points-with-d3.html">
+                Rendering One Million Datapoints with D3 and WebGL by Scott
+                Logic
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
