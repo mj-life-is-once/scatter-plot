070546027653fafe75514a0c41539026b87ae67a
Minjoo Cho <minjoolisa.cho@gmail.com>
2023-09-22 14:11:00 +0100

    solved data issues

--- src/app/components/WebGLChart.tsx
+++ src/app/components/WebGLChart.tsx
@@ -20,9 +20,9 @@ interface ChartProps {
   className?: string;
 }
 const WebGLChart = (props: ChartProps) => {
-  const [data, setData] = useState(props.data);
-  const [callDraw, setCallDraw] = useState(false);
-  const chartRef = useRef<unknown>(null);
+  const [bigData, setBigData] = useState(props.data);
+  // const [callDraw, setCallDraw] = useState(false);
+  const chartRef = useRef<any>(null);
 
   const quadtree = useMemo(
     () =>
@@ -30,20 +30,14 @@ const WebGLChart = (props: ChartProps) => {
         .quadtree()
         .x((d: any) => d.x)
         .y((d: any) => d.y)
-        .addAll(data),
-    [data]
+        .addAll(bigData),
+    [bigData]
   );
 
   const xScale = useMemo(() => d3.scaleLinear().domain([-50, 50]), []);
   const yScale = useMemo(() => d3.scaleLinear().domain([-50, 50]), []);
   const xScaleOriginal = useMemo(() => xScale.copy(), [xScale]);
   const yScaleOriginal = useMemo(() => yScale.copy(), [yScale]);
-
-  // const annotationSeries = seriesSvgAnnotation()
-  //   .notePadding(15)
-  //   .type(d3.annotationCallout);
-
-  // const annotations: any[] = [];
 
   const pointSeries = useMemo(
     () =>
@@ -58,113 +52,113 @@ const WebGLChart = (props: ChartProps) => {
     []
   );
 
-  const redraw = useCallback(
-    async (data: any, chart: any) => {
-      // Pass over data by parameter, otherwise it would only update the subsets of data
-      console.log("redraw", data.length);
-      d3.select("#chart").datum(data).call(chart);
-    },
-    [callDraw]
-  );
-
   const chart = useMemo(
     () =>
-      fc
-        .chartCartesian(xScale, yScale)
-        .webglPlotArea(fc.seriesWebglMulti().series([pointSeries]))
-        //   .svgPlotArea(
-        //     // only render the annotations series on the SVG layer
-        //     fc
-        //       .seriesSvgMulti()
-        //       .series([annotationSeries])
-        //       .mapping((d: any) => d.annotations)
-        //   )
-        .decorate((sel) =>
-          sel
-            .enter()
-            .selectAll(".plot-area")
-            .on("measure.range", (event: any) => {
-              xScaleOriginal.range([0, event.detail.width]);
-              yScaleOriginal.range([event.detail.height, 0]);
-            })
-            .call(
-              d3
-                .zoom()
-                .scaleExtent([0.8, 10])
-                .on("zoom", (event: any) => {
-                  // update the scales based on current zoom
-                  // console.log("zoom called");
-                  xScale.domain(
-                    event.transform.rescaleX(xScaleOriginal).domain()
-                  );
-                  yScale.domain(
-                    event.transform.rescaleY(yScaleOriginal).domain()
-                  );
-                  redraw(data, chart);
-                  // setCallDraw((prev) => !prev);
-                }) as any
-            )
-            .call(
-              fc.pointer().on("point", ([coord]: any[]) => {
-                //   annotations.pop();
+      fc.chartCartesian(xScale, yScale).webglPlotArea(
+        // only render the point series on the WebGL layer
+        fc
+          .seriesWebglMulti()
+          .series([pointSeries])
+          .mapping((d: any) => {
+            console.log("webplotArea", d.data.length);
+            return d.data;
+          })
+      ),
+    [pointSeries, xScale, yScale]
+  )
+    // .svgPlotArea(
+    //   // only render the annotations series on the SVG layer
+    //   fc
+    //     .seriesSvgMulti()
+    //     .series([annotationSeries])
+    //     .mapping(d => d.annotations)
+    // )
+    .decorate((sel: any) =>
+      sel
+        .enter()
+        .selectAll(".plot-area")
+        .on("measure.range", (event: any) => {
+          xScaleOriginal.range([0, event.detail.width]);
+          yScaleOriginal.range([event.detail.height, 0]);
+        })
+        .call(zoom as any)
+        .call(
+          fc.pointer().on("point", ([coord]: any[]) => {
+            //   annotations.pop();
 
-                if (!coord || !quadtree) {
-                  return;
-                }
+            if (!coord || !quadtree) {
+              return;
+            }
 
-                // find the closes datapoint to the pointer
-                const x = xScale.invert(coord.x);
-                const y = yScale.invert(coord.y);
-                const radius = Math.abs(
-                  xScale.invert(coord.x) - xScale.invert(coord.x - 20)
-                );
-                const closestDatum = quadtree.find(
-                  x,
-                  y,
-                  radius
-                ) as unknown as HathiData;
+            // find the closes datapoint to the pointer
+            const x = xScale.invert(coord.x);
+            const y = yScale.invert(coord.y);
+            const radius = Math.abs(
+              xScale.invert(coord.x) - xScale.invert(coord.x - 20)
+            );
 
-                // if the closest point is within 20 pixels, show the annotation
-                if (closestDatum) {
-                  console.log(
-                    "closestDatum",
-                    closestDatum.title,
-                    closestDatum.x,
-                    closestDatum.y
-                  );
-                  // only draw when there's data to show -> separate the canvas
-                  // setCallDraw((prev) => !prev);
-                  console.log(data.length);
-                  redraw(data, chart);
-                }
-              })
-            )
-        ),
-    [
-      data,
-      pointSeries,
-      quadtree,
-      redraw,
-      xScale,
-      xScaleOriginal,
-      yScale,
-      yScaleOriginal,
-    ]
+            const closestDatum = quadtree.find(
+              x,
+              y,
+              radius
+            ) as unknown as HathiData;
+
+            // if the closest point is within 20 pixels, show the annotation
+            if (closestDatum) {
+              console.log(
+                "closestDatum",
+                // closestDatum.title,
+                closestDatum.x,
+                closestDatum.y
+              );
+
+              // only draw when there's data to show -> separate the canvas
+              // setCallDraw((prev) => !prev);
+              //console.log(data, data.bigData.length);
+              //redraw();
+            }
+          })
+        )
+    );
+
+  const redraw: any = useCallback(
+    async (data: any) => {
+      // Pass over data by parameter, otherwise it would only update the subsets of data
+      console.log("redraw", data.length);
+      chartRef.current.datum({ data }).call(chart);
+    },
+    [chart]
   );
 
+  const zoom = useMemo(
+    () =>
+      d3
+        .zoom()
+        .scaleExtent([0.8, 10])
+        .on("zoom", (event: any, d: any) => {
+          // update the scales based on current zoom
+          // console.log("zoom called");
+          xScale.domain(event.transform.rescaleX(xScaleOriginal).domain());
+          yScale.domain(event.transform.rescaleY(yScaleOriginal).domain());
+
+          console.log("on zoom", d.data.length);
+          redraw(d.data);
+          // setCallDraw((prev) => !prev);
+        }),
+    [redraw, xScale, xScaleOriginal, yScale, yScaleOriginal]
+  );
   useEffect(() => {
-    console.log("data", data);
-  }, [data]);
+    console.log("data", bigData);
+  }, [bigData]);
 
   useEffect(() => {
     console.log("set data called");
-    setData(props.data);
+    setBigData(props.data);
   }, [props.data]);
 
   useEffect(() => {
     // console.log(props.data);
     chartRef.current = d3.select("#chart");
-
     const languageColorScale = d3.scaleOrdinal(d3.schemeCategory10);
     const yearColorScale = d3
       .scaleSequential()
@@ -176,24 +170,31 @@ const WebGLChart = (props: ChartProps) => {
 
     const yearFill = (d: any) => webglColor(yearColorScale(d.year));
 
-    const fillColor = fc.webglFillColor().value(languageFill).data(data);
+    const fillColor = fc.webglFillColor().value(languageFill).data(bigData);
     pointSeries.decorate((program: any) => fillColor(program));
 
     // wire up the fill color selector
-    iterateElements(".controls a", (el: any) => {
-      el.addEventListener("click", () => {
-        iterateElements(".controls a", (el2: any) =>
-          el2.classList.remove("active")
-        );
-        el.classList.add("active");
-        fillColor.value(el.id === "language" ? languageFill : yearFill);
-        redraw(data, chart);
-      });
-    });
-    redraw(data, chart);
-  }, [chart, data, pointSeries, redraw]);
+    // iterateElements(".controls a", (el: any) => {
+    //   el.addEventListener("click", () => {
+    //     iterateElements(".controls a", (el2: any) =>
+    //       el2.classList.remove("active")
+    //     );
+    //     el.classList.add("active");
+    //     fillColor.value(el.id === "language" ? languageFill : yearFill);
+    //     redraw(bigData);
+    //   });
+    // });
+    redraw(bigData);
+  }, [bigData, chart, pointSeries, redraw]);
   return (
     <>
+      {/* <button
+        onClick={() => {
+          setCallDraw((prev) => !prev);
+        }}
+      >
+        redraw
+      </button> */}
       <div id="chart" className={`relative ${props.className ?? ""}`}>
         {/* <div className="absolute top-0 left-0">
           <svg width="100" height="50">
